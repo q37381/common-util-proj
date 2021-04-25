ab7fa3e9523a09bd590426a53d92ddb778994248
W <373814603@qq.com>
2021-04-25 22:16:57 +0800

    解析数据库表格

--- sql-util/src/main/java/com/common/util/MeiDocSqlUtil.java
+++ sql-util/src/main/java/com/common/util/MeiDocSqlUtil.java
@@ -34,7 +34,8 @@ public class MeiDocSqlUtil {
 
         LOGGER.debug("传入待分割字符串");
         LOGGER.debug("{}", doc);
-        String[] splitDocArr = splitDoc(doc);
+        
+        String[] splitDocArr = splitDoc(doc.toLowerCase());
 
         List<List<String>> list = transToListString(splitDocArr);
 
@@ -53,29 +54,82 @@ public class MeiDocSqlUtil {
 
         List<SqlFieldDefinition> definitionlist = Lists.newArrayList();
 
-        boolean tableFind = false;
+        int tableLine = -1;
 
         for (int i = 0; i < list.size(); i++) {
             List<String> row = list.get(i);
+
             // 非字段行
             if (row.size() < 4) {
                 for (int j = 0; j < row.size(); j++) {
-                    String line = row.get(j);
+                    String column = row.get(j);
 
-                    // 找表名
-                    if (!tableFind) {
-                        SqlFieldDefinition definition = searchTableGetDefinition(line);
+                    // 找表名，最后一列
+                    if (tableLine == -1) {
+                        SqlFieldDefinition definition = searchTableGetDefinition(row.get(row.size() - 1));
+
+                        // 找到表了 继续找表的注释，如果有的话
                         if (definition != null) {
                             definitionlist.add(definition);
+                            tableLine = i;
+
+                            int findLine = searchTableAndSetComment(tableLine, list, definition);
+                            // 找到了就跳到那一行，下次就从这行的下一行继续找其他的
+                            if (findLine > i) {
+                                i = findLine;
+                            }
+
+                            break;
                         }
                     }
 
-                    searchIndexSetDefinition(definitionlist, line);
+                    // 找索引字段
+                    searchIndexSetDefinition(definitionlist, column);
                 }
-            } else {
 
+            } else {
+                // 字段行
             }
         }
+    }
+
+    /**
+     * 
+     * @param tableLine
+     * @param list
+     * @param definition
+     * @return 表注释在哪一行 0开始
+     */
+    private static int searchTableAndSetComment(int tableLine, List<List<String>> list, SqlFieldDefinition definition) {
+        // -1代表没找到
+        int findLine = -1;
+        // 表注释在下方
+        if (tableLine > 0) {
+            List<String> rowUp1 = list.get(tableLine - 1);
+            // 取一行最末一列来判断
+            if (searchTableCommentSetName(definition, rowUp1.get(rowUp1.size() - 1))) {
+                findLine = tableLine - 1;
+            }
+        }
+
+        if (findLine == -1) {
+            List<String> rowUp1 = list.get(tableLine + 1);
+            // 取一行最末一列来判断
+            if (searchTableCommentSetName(definition, rowUp1.get(rowUp1.size() - 1))) {
+                findLine = tableLine + 1;
+            }
+        }
+
+        return findLine;
+    }
+
+    private static boolean searchTableCommentSetName(SqlFieldDefinition tableDefinition, String column) {
+        if (textIsNotIndex(column)) {
+            tableDefinition.setName(column);
+            return true;
+        }
+
+        return false;
     }
 
     /**
@@ -92,46 +146,43 @@ public class MeiDocSqlUtil {
      * TODOM 目前就这仨
      * 
      * @param definitionlist
-     * @param line
+     * @param column
      */
-    private static void searchIndexSetDefinition(List<SqlFieldDefinition> definitionlist, String line) {
-        if (line.indexOf(SQLFieldTypeEnum.PRIMARY_KEY.getSearchName()) != -1
-                || line.indexOf(SQLFieldTypeEnum.UNIQUE_INDEX.getSearchName()) != -1
-                || line.indexOf(SQLFieldTypeEnum.INDEX.getSearchName()) != -1) {
-
-            String[] split = line.split("\\r\\n");
-            if (split == null) {
-                split = line.split("[\\r\\n]");
-            }
-
-            // 具体哪一个类型索引
-            for (String one : split) {
-                SqlFieldDefinition definition = null;
-                Matcher matcher = INDEX_PATTERN.matcher(one);
-                if (!matcher.find()) {
-                    LOGGER.info("没有匹配到任何索引字段，字符串[{}]", one);
-                }
-                if (one.indexOf(SQLFieldTypeEnum.PRIMARY_KEY.getSearchName()) != -1) {
-                    definition = new SqlFieldDefinition(null, matcher.group(1), SQLFieldTypeEnum.PRIMARY_KEY);
-                }
-                if (one.indexOf(SQLFieldTypeEnum.UNIQUE_INDEX.getSearchName()) != -1) {
-                    definition = new SqlFieldDefinition(matcher.group(1), matcher.group(2),
-                            SQLFieldTypeEnum.UNIQUE_INDEX);
-                }
-                if (one.indexOf(SQLFieldTypeEnum.INDEX.getSearchName()) != -1) {
-                    definition = new SqlFieldDefinition(matcher.group(1), matcher.group(2), SQLFieldTypeEnum.INDEX);
-                }
-
-                definitionlist.add(definition);
-            }
+    private static void searchIndexSetDefinition(List<SqlFieldDefinition> definitionlist, String column) {
+        if (textIsNotIndex(column)) {
+            return;
         }
+
+        // 具体哪一个类型索引
+        SqlFieldDefinition definition = null;
+        Matcher matcher = INDEX_PATTERN.matcher(column);
+        if (!matcher.find()) {
+            LOGGER.info("没有匹配到任何索引字段，字符串[{}]", column);
+        }
+        if (column.indexOf(SQLFieldTypeEnum.PRIMARY_KEY.getSearchName()) != -1) {
+            definition = new SqlFieldDefinition(null, matcher.group(1), SQLFieldTypeEnum.PRIMARY_KEY);
+        }
+        if (column.indexOf(SQLFieldTypeEnum.UNIQUE_INDEX.getSearchName()) != -1) {
+            definition = new SqlFieldDefinition(matcher.group(1), matcher.group(2), SQLFieldTypeEnum.UNIQUE_INDEX);
+        }
+        if (column.indexOf(SQLFieldTypeEnum.INDEX.getSearchName()) != -1) {
+            definition = new SqlFieldDefinition(matcher.group(1), matcher.group(2), SQLFieldTypeEnum.INDEX);
+        }
+
+        definitionlist.add(definition);
 
     }
 
-    private static SqlFieldDefinition searchTableGetDefinition(String line) {
-        if (line.indexOf(DATABASE_PREFIX) != -1 || line.indexOf(TABLE_PREFIX) != -1) {
+    private static boolean textIsNotIndex(String text) {
+        return text.indexOf(SQLFieldTypeEnum.PRIMARY_KEY.getSearchName()) == -1
+                && text.indexOf(SQLFieldTypeEnum.UNIQUE_INDEX.getSearchName()) == -1
+                && text.indexOf(SQLFieldTypeEnum.INDEX.getSearchName()) == -1;
+    }
+
+    private static SqlFieldDefinition searchTableGetDefinition(String column) {
+        if (column.indexOf(DATABASE_PREFIX) != -1 || column.indexOf(TABLE_PREFIX) != -1) {
             // 该行为表名
-            SqlFieldDefinition definition = new SqlFieldDefinition(line.trim(), SQLFieldTypeEnum.TABLE, null, null);
+            SqlFieldDefinition definition = new SqlFieldDefinition(column.trim(), SQLFieldTypeEnum.TABLE, null, null);
             // 找该表的备注，可能在之前可能在之后，依情况而定
             // TODOM 这里先不存表名
             return definition;
@@ -156,7 +207,8 @@ public class MeiDocSqlUtil {
             }
 
             // 第二次切分一行中的列
-            String[] split2 = first.split("[\\t ]");
+            // 不能分割unique index， primary key，index 之间的空格
+            String[] split2 = first.split("(\\t|(?<!unique) (?!key|\\(|\\`))");
             List<String> columnList = Lists.newArrayList();
 
             LOGGER.debug("第二次切分，切分出每行的列：");
@@ -259,12 +311,16 @@ public class MeiDocSqlUtil {
 
         while (matcher.find()) {
             for (int i = 1; i <= matcher.groupCount(); i++) {
-                System.out.println(i + ":" + matcher.group(i));
+                //System.out.println(i + ":" + matcher.group(i));
             }
         }
 
-        str = "   fsdfdsfsdadasddasdasdasd\r\n" + "dsad\tdasdad\tdasd\tsdas\r\n" + "123 2312    123 3123\r\n" + "";
+        str = "asdas db_ddd.t_ddd\r\n"
+                + "sadasd 的雷克萨进度款拉丝机大数据库里\r\n"
+                + "dasd\tprimary key (`id`),\r\n"
+                + "UNIQUE INDEX `uk_parent_id` (`parent_id`),\r\n"
+                + "index `idx_parent_id` (`parent_id`)";
 
-        // splitWord(str);
+         splitWord(str);
     }
 }
